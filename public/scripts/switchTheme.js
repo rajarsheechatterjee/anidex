787fe0dc934c0d17b7a3f2ae47de1b369fe24edd
Rajarshee Chatterjee <rajarshee.adm@gmail.com>
2020-08-17 19:12:46 +0530

    Add dark theme toggle switch

--- public/scripts/switchTheme.js
+++ public/scripts/switchTheme.js
@@ -1,9 +1,20 @@
-function switchTheme() {
-    localStorage.setItem('mode', (localStorage.getItem('mode') || 'dark') === 'dark' ? 'light' : 'dark');
+function loadTheme() {
+    const checkBox = document.getElementById("customSwitch1");
 
-    localStorage.getItem('mode') === 'dark' ? (
-        document.querySelector('body').classList.add('dark')
-    ) : (
-        document.querySelector('body').classList.remove('dark')
-    );
+    if (localStorage.getItem('mode') === 'dark') {
+        document.querySelector('body').classList.add('dark');
+        checkBox.checked = true;
+    }
+}
+
+function switchTheme2() {
+    const checkBox = document.getElementById("customSwitch1");
+
+    if (checkBox.checked == true){
+        localStorage.setItem('mode','dark');
+        document.querySelector('body').classList.add('dark');
+    } else {
+        localStorage.setItem('mode','light');
+        document.querySelector('body').classList.remove('dark');
+    }
 }
